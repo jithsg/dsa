41c0463a393699f001403878e3540c84d482ca6a
jithsg <135303624+jithsg@users.noreply.github.com>
2025-01-17 21:53:50 +0800

    bfs search

--- BFS-path.py
+++ BFS-path.py
@@ -14,21 +14,50 @@ class Graph:
             
             for neighbor in neighbors:
                 if neighbor not in visited and neighbor not in to_visit:
-                    to_visit.append(neighbor)
-                    
+                    to_visit.append(neighbor)       
         return visited
+    
+    def bfs_search(self, start, end):
+        visited =[]
+        to_visit= []
+        to_visit.append(start)
+        path={start: None}
+        
+        while to_visit:
+            current = to_visit.pop(0)
+            visited.append(current)
+            
+            neighbors = sorted(self.graph[current])
+            
+            for neighbor in neighbors:
+                if neighbor not in visited and neighbor not in to_visit:
+                    to_visit.append(neighbor)
+                    path[neighbor]= current
+                    
+        path_list=[]
+        current = end
+            
+        while current is not None:
+            path_list.append(current)
+            current = path[current]
+        path_list.reverse()
+        return path_list
+                
+            
+        
         
         
 g = Graph()
-
 g.graph = {
-    "A" : {"B", "C"},
-    "B" : {"D"},
-    "C" : {"A"},
-    "D" : {"E"},
-    "E" : {"F"},
-    "F" : {"E"}
-}
+       "A":{"B", "C"},
+      "B" : {"D"},
+       "C" : {"A"},
+       "D" : {"B", "E"},
+       "E":{"F"},
+       "F":{"E"}
+      }
 
-print(g.bfs("A"))
+# Let's run BFS starting from vertex 'A'
+result = g.bfs_search('A', 'F')
+print(result)
 
