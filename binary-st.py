386f052be39234ca51f51127cba7d60a1ad85ebc
jithsg <135303624+jithsg@users.noreply.github.com>
2025-02-04 01:34:00 +0800

    first

---
+++ binary-st.py
@@ -0,0 +1,51 @@
+class BST:
+    def __init__(self, value, depth=1):
+        self.value = value
+        self.depth = depth
+        self.left = None
+        self.right= None
+        
+    def insert(self, value):
+        
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value, self.depth+1)
+                print(f'Tree node {value} added to the left of {self.value} at depth {self.depth + 1}')
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value, self.depth+1)
+                print(f'Tree node {value} added to the right of {self.value} at depth {self.depth + 1}')
+            else:
+                self.right.insert(value)
+                
+    def get_node_by_value(self, value):
+        if self.value == value:
+            return self
+        
+        elif value < self.value and self.left:
+            return self.left.get_node_by_value(value)
+        
+        elif value > self.value and self.right:
+            return self.right.get_node_by_value(value)
+        else:
+            return None
+    
+    def in_order(self):
+        if self.left is not None:
+            self.left.in_order()
+        print(f'Depth= {self.depth} and Value {self.value}')
+        if self.left is not None:
+            self.right.in_order()
+        
+        
+        
+                
+b= BST(100)
+b.insert(50)
+b.insert(125)
+b.insert(25)
+b.insert(75)
+
+print(b.in_order())
