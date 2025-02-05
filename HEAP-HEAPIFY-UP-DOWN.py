390bc5933c4e6b768459f595c77dde0e2484d772
jithsg <135303624+jithsg@users.noreply.github.com>
2025-02-06 01:51:50 +0800

    heapsort

--- HEAP-HEAPIFY-UP-DOWN.py
+++ HEAP-HEAPIFY-UP-DOWN.py
@@ -42,13 +42,26 @@ def heapify(arr):
     for i in range(n//2 - 1, -1, -1):
         heapify_down(arr, n, i)
     return arr
+
+def heapsort(arr):
+    n= len(arr)
     
+    for i in range(n//2-1, -1, -1):
+        heapify_down(arr, len(arr), i)
+        
+    for i in range(n-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify_down(arr, i, 0)
+    return arr
     
 # arr = [1, 3, 6, 5, 9, 8, 10]
 arr=[10, 5, 8, 3, 1, 9, 4]
 
-print("Original array:", arr)
-print("After heapify:", heapify(arr))
+print(heapsort(arr))
+# print("Original array:", arr)
+# print("After heapify:", heapify(arr))
+
+
     
     
         
