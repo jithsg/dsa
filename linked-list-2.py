5abdf58b7fe745eea9dd0ab6a7bb7d72e4bd3348
jithsg <135303624+jithsg@users.noreply.github.com>
2025-02-11 03:56:10 +0800

    first

---
+++ linked-list-2.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
+        
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+        
+    def add_begin(self, data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+    
+    def traverse(self):
+        if self.head is None:
+            print("List is empty")
+        current = self.head
+        while current is not None:
+            print(current.data, end=" -> ")
+            current = current.ref
+            
+    def add_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+            
+        current = self.head
+        while current.ref is not None:
+            current = current.ref
+  
+        current.ref = new_node
+        
+    def add_after(self, x, data):
+        
+        new_node = Node(data)
+        current = self.head
+        
+        if self.head is None:
+            print("Linked list is empty, cannot add after the given node.")
+            return
+            
+        while current is not None:
+            if current.data == x:
+                new_node.ref = current.ref
+                current.ref = new_node
+                return
+            current= current.ref
+            
+    def add_before(self, x, data):
+        new_node = Node(data)
+        current = self.head
+        
+        
+        if self.head is None:
+            print("Linked list is empty, cannot add before the given node.")
+            return
+        if self.head.data ==x:
+            new_node.ref = self.head
+            self.head = new_node
+            return 
+    
+        
+        while current.ref is not None:
+            if current.ref.data == x:
+                new_node.ref= current.ref
+                current.ref = new_node
+                return 
+            current = current.ref
+        print(f"Node with value {x} not found.")
+                
+ll= Linkedlist()
+ll.add_begin(3)
+ll.add_begin(2)
+ll.add_begin(1)
+ll.add_after(3,4)
+ll.add_before(1, 0)
+ll.traverse()
