732a1009932e234028c5fc85edde6b01266c4ff8
jithsg <135303624+jithsg@users.noreply.github.com>
2025-02-11 20:51:52 +0800

    second

--- linked-list-2.py
+++ linked-list-2.py
@@ -70,6 +70,50 @@ class Linkedlist:
                 return 
             current = current.ref
         print(f"Node with value {x} not found.")
+        
+    def delete_begin(self):
+        if self.head is None:
+            print("Linkedlist is empty")
+            return 
+        
+        self.head = self.head.ref
+        
+    def delete_last(self):
+        if self.head is None:
+            print("Linkedlist is empy")
+            return
+        if self.head.ref is None: #one node
+            self.head =None
+            return
+        current = self.head
+        while current.ref.ref is not None:
+            current = current.ref
+        current.ref= None
+                
+    def delete_by_value(self, x):
+        if self.head is None:
+            print("Linkedlist is empy")
+            return
+        
+        if self.head.data== x:
+            self.head = self.head.ref
+    
+        
+        current = self.head
+        
+        while current.ref is not None:
+            if current.ref.data == x:
+                current.ref = current.ref.ref
+            current = current.ref
+            
+        if current is None:
+            print(f"Node with value {x} not found.")
+                
+    
+        
+            
+            
+        
                 
 ll= Linkedlist()
 ll.add_begin(3)
@@ -77,4 +121,6 @@ ll.add_begin(2)
 ll.add_begin(1)
 ll.add_after(3,4)
 ll.add_before(1, 0)
+
+ll.delete_by_value(1)
 ll.traverse()
